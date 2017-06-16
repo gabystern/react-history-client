8b05ff654ff2b2c06e304a57b9e98a58e8da0a3f
gabystern <stern.gabrielle@gmail.com>
2017-06-16 16:07:21 -0400

    topic cards css

--- src/Components/TopicBox.js
+++ src/Components/TopicBox.js
@@ -4,17 +4,14 @@ import { Link } from 'react-router-dom'
 const TopicBox = ({ topic }) => {
   return (
     <div className="row">
-       <div className="col s12 m7">
+       <div className="col s12 m4 center-align">
          <div className="card">
            <div className="card-image">
-             <img src={topic.title_medium.url} />
+             <img className="image" src={topic.title_medium.url} />
              <span className="card-title">{topic.name}</span>
            </div>
            <div className="card-content">
              <p>{topic.title_text.text}</p>
-           </div>
-           <div className="card-action">
-           <Link to={`/${topic.id}`}>Go to {topic.name}s Timeline </Link>
            </div>
          </div>
        </div>
