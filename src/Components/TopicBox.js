7ec64af5449cb0b1a655a177ab8ad1bcf75e58c2
aisleypay <paisley.singh@gmail.com>
2017-06-16 14:44:59 -0400

    scrape time line

--- src/Components/TopicBox.js
+++ src/Components/TopicBox.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import TopicTimeline from './TopicTimeline'
 import { Link } from 'react-router-dom'
 
 const TopicBox = ({ topic }) => {
--- src/Components/TopicTimeline.js
+++ src/Components/TopicTimeline.js
@@ -1,10 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Parallax } from 'react-parallax';
 
-const TopicTimeline = ({ topic }) => {
-  console.log("Hello")
-  return null
+export default class TopicTimeline extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      topic: ''
+    }
+  }
+
+  makeEventsJson() {
+    const events = this.props.topic.events
+
+    let event_array = events.map(e => {
+      return {
+        media: {
+          url: e.event_media.url,
+          caption: e.event_media.caption
+        },
+        start_date: {
+          year: e.event_start_date.year
+        },
+        text: {
+          headline: e.event_text.headline,
+          text: e.event_text.text
+        }
+      }
+    })
+
+    return event_array;
+  }
+
+  makeJson() {
+    const topic = this.props.topic
+    return {
+      title: {
+        media: {
+          url: topic.text_medium.url,
+          caption: topic.title_medium.caption
+        },
+        text: {
+          headline: topic.title_text.headline,
+          text: topic.title_text.text
+        }
+      },
+      events: this.makeEventsJson()
+    }
+  }
+  // let timeline_json = make_the_json(); // you write this part
+  // // two arguments: the id of the Timeline container (no '#')
+  // // and the JSON object or an
+
+
+  render() {
+    return (
+      <div>
+        <Parallax bgImage='{this.props.topic.events[0].event_medium.url}' strength={400}>
+          <br/>
+          <h1> some content that is displayed above the bgImage </h1>
+        </Parallax>
+      </div>
+    )
+  }
 }
-
-export default TopicTimeline;
 
 //  edit route
